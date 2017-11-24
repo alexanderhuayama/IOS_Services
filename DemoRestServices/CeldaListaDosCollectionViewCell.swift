7cec6a54fffbbb7c2984c279a59eebcb57100931
Giovanni Alexander <alexander.hh.cc@gmail.com>
2017-11-24 09:43:08 -0500

    Cambio

---
+++ DemoRestServices/CeldaListaDosCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CeldaListaDosCollectionViewCell.swift
+//  DemoRestServices
+//
+//  Created by Giovanni on 11/24/17.
+//  Copyright © 2017 Francisco Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class CeldaListaDosCollectionViewCell: UICollectionViewCell {
+    var itemLista: Dictionary<String, AnyObject>!
+    
+    func iniciarCelda(){
+        
+    }
+}
--- DemoRestServices/ListaDosViewController.swift
+++ DemoRestServices/ListaDosViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ListaDosViewController: UIViewController {
 
+    @IBOutlet weak var coleccion: UICollectionView!
+    var listData:   NSMutableArray = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let url: String = "http://www.andina.com.pe/WSAndina/AndinaService.svc/GetNoticiasSeccion"
+        
+        var body = Dictionary<String, AnyObject>()
+        body["seccionid"] = 17 as AnyObject
 
+        
+        if(NetworkManager.isConnectedToNetwork()){
+            NetworkManager.sharedInstance.callUrlWithCompletion(url: url, params: body, completion: {
+                (finished, response) in
+                if(finished){
+                    let json = response["GetNoticiasSeccionResult"] as! Dictionary<String, AnyObject>
+                    let array = NSMutableArray(array: json["Result"] as! NSArray)
+                    self.listData = array
+                    self.coleccion.reloadData()
+                }else{
+                    // Error de conexion
+                }
+            }, method: .post)
+        }else{
+            // indica que no hay internet
+        
+        }
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -31,5 +58,43 @@ class ListaDosViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+   
 
+}
+
+extension ListaDosViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    //
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    //
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.listData.count
+    }
+    
+    //
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celda = collectionView.dequeueReusableCell(withReuseIdentifier: "CeldaListaDosCollectionViewCell", for: indexPath) as! CeldaListaDosCollectionViewCell
+        
+        celda.itemLista = self.listData[indexPath.row] as! Dictionary<String, AnyObject>
+        celda.iniciarCelda()
+        return celda
+    }
+    
+    //
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let w = (collectionView.frame.width - 30) / 2
+        let h = w * 1.5
+        return CGSize(width: w, height: h)
+    }
+    
+    //
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let itemData = self.listData[indexPath.row] as! Dictionary<String, AnyObject>
+        
+    }
+    
 }
